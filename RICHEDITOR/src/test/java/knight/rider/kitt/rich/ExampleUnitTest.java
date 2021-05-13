18c92e855272eed849469e5fd4271a12ab094830
Knight-Rider888 <80495691+Knight-Rider888@users.noreply.github.com>
2021-05-13 21:36:40 +0800

    1.0.1

--- RICHEDITOR/src/test/java/knight/rider/kitt/rich/ExampleUnitTest.java
+++ RICHEDITOR/src/test/java/knight/rider/kitt/rich/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package knight.rider.kitt.rich;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
